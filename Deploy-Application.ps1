0b8359029968ff9ff608072eaba78fc7993630c1
Jordan Hamilton <jordan-hamilton@@users.noreply.github.com>
2018-01-08 16:56:35 -0700

    Initial version

---
+++ Deploy-Application.ps1
@@ -0,0 +1,228 @@
+﻿<#
+.SYNOPSIS
+	This script performs the installation or uninstallation of an application(s).
+.DESCRIPTION
+	The script is provided as a template to perform an install or uninstall of an application(s).
+	The script either performs an "Install" deployment type or an "Uninstall" deployment type.
+	The install deployment type is broken down into 3 main sections/phases: Pre-Install, Install, and Post-Install.
+	The script dot-sources the AppDeployToolkitMain.ps1 script which contains the logic and functions required to install or uninstall an application.
+.PARAMETER DeploymentType
+	The type of deployment to perform. Default is: Install.
+.PARAMETER DeployMode
+	Specifies whether the installation should be run in Interactive, Silent, or NonInteractive mode. Default is: Interactive. Options: Interactive = Shows dialogs, Silent = No dialogs, NonInteractive = Very silent, i.e. no blocking apps. NonInteractive mode is automatically set if it is detected that the process is not user interactive.
+.PARAMETER AllowRebootPassThru
+	Allows the 3010 return code (requires restart) to be passed back to the parent process (e.g. SCCM) if detected from an installation. If 3010 is passed back to SCCM, a reboot prompt will be triggered.
+.PARAMETER TerminalServerMode
+	Changes to "user install mode" and back to "user execute mode" for installing/uninstalling applications for Remote Destkop Session Hosts/Citrix servers.
+.PARAMETER DisableLogging
+	Disables logging to file for the script. Default is: $false.
+.EXAMPLE
+    powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeployMode 'Silent'; Exit $LastExitCode }"
+.EXAMPLE
+    powershell.exe -Command "& { & '.\Deploy-Application.ps1' -AllowRebootPassThru; Exit $LastExitCode }"
+.EXAMPLE
+    powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeploymentType 'Uninstall'; Exit $LastExitCode }"
+.EXAMPLE
+    Deploy-Application.exe -DeploymentType "Install" -DeployMode "Silent"
+.NOTES
+	Toolkit Exit Code Ranges:
+	60000 - 68999: Reserved for built-in exit codes in Deploy-Application.ps1, Deploy-Application.exe, and AppDeployToolkitMain.ps1
+	69000 - 69999: Recommended for user customized exit codes in Deploy-Application.ps1
+	70000 - 79999: Recommended for user customized exit codes in AppDeployToolkitExtensions.ps1
+.LINK
+	http://psappdeploytoolkit.com
+#>
+[CmdletBinding()]
+Param (
+	[Parameter(Mandatory=$false)]
+	[ValidateSet('Install','Uninstall')]
+	[string]$DeploymentType = 'Install',
+	[Parameter(Mandatory=$false)]
+	[ValidateSet('Interactive','Silent','NonInteractive')]
+	[string]$DeployMode = 'Interactive',
+	[Parameter(Mandatory=$false)]
+	[switch]$AllowRebootPassThru = $false,
+	[Parameter(Mandatory=$false)]
+	[switch]$TerminalServerMode = $false,
+	[Parameter(Mandatory=$false)]
+	[switch]$DisableLogging = $false
+)
+
+Try {
+	## Set the script execution policy for this process
+	Try { Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force -ErrorAction 'Stop' } Catch { Write-Error "Failed to set the execution policy to Bypass for this process." }
+
+	##*===============================================
+	##* VARIABLE DECLARATION
+	##*===============================================
+	## Variables: Application
+	[string]$appVendor = 'Oracle'
+	[string]$appName = 'NetBeans IDE'
+	[string]$appVersion = '8.2'
+	[string]$appArch = 'x64'
+	[string]$appLang = 'EN'
+	[string]$appRevision = '01'
+	[string]$appScriptVersion = '1.0.0'
+	[string]$appScriptDate = '01/03/2018'
+	[string]$appScriptAuthor = 'Jordan Hamilton'
+	##*===============================================
+	## Variables: Install Titles (Only set here to override defaults set by the toolkit)
+	[string]$installName = ''
+	[string]$installTitle = ''
+
+	##* Do not modify section below
+	#region DoNotModify
+
+	## Variables: Exit Code
+	[int32]$mainExitCode = 0
+
+	## Variables: Script
+	[string]$deployAppScriptFriendlyName = 'Deploy Application'
+	[version]$deployAppScriptVersion = [version]'3.6.9'
+	[string]$deployAppScriptDate = '02/12/2017'
+	[hashtable]$deployAppScriptParameters = $psBoundParameters
+
+	## Variables: Environment
+	If (Test-Path -LiteralPath 'variable:HostInvocation') { $InvocationInfo = $HostInvocation } Else { $InvocationInfo = $MyInvocation }
+	[string]$scriptDirectory = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
+
+	## Dot source the required App Deploy Toolkit Functions
+	Try {
+		[string]$moduleAppDeployToolkitMain = "$scriptDirectory\AppDeployToolkit\AppDeployToolkitMain.ps1"
+		If (-not (Test-Path -LiteralPath $moduleAppDeployToolkitMain -PathType 'Leaf')) { Throw "Module does not exist at the specified location [$moduleAppDeployToolkitMain]." }
+		If ($DisableLogging) { . $moduleAppDeployToolkitMain -DisableLogging } Else { . $moduleAppDeployToolkitMain }
+	}
+	Catch {
+		If ($mainExitCode -eq 0){ [int32]$mainExitCode = 60008 }
+		Write-Error -Message "Module [$moduleAppDeployToolkitMain] failed to load: `n$($_.Exception.Message)`n `n$($_.InvocationInfo.PositionMessage)" -ErrorAction 'Continue'
+		## Exit the script, returning the exit code to SCCM
+		If (Test-Path -LiteralPath 'variable:HostInvocation') { $script:ExitCode = $mainExitCode; Exit } Else { Exit $mainExitCode }
+	}
+
+	#endregion
+	##* Do not modify section above
+	##*===============================================
+	##* END VARIABLE DECLARATION
+	##*===============================================
+
+	If ($deploymentType -ine 'Uninstall') {
+		##*===============================================
+		##* PRE-INSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Pre-Installation'
+
+		## Show Welcome Message, close applications if required, allow up to 3 deferrals, verify there is enough disk space to complete the install, and persist the prompt
+		Show-InstallationWelcome -CloseApps 'iexplore,firefox,netbeans64' -CheckDiskSpace -PersistPrompt
+
+		## Show Progress Message (with the default message)
+		Show-InstallationProgress
+
+		## <Perform Pre-Installation tasks here>
+		## Remove existing Java versions
+		$exitCode = Remove-MSIApplications -Name "Java 8 Update" -PassThru
+		If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
+		$exitCode = Remove-MSIApplications -Name "Java 7 Update" -PassThru
+		If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
+		$exitCode = Remove-MSIApplications -Name "Java SE Development Kit 8 Update" -PassThru
+		If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
+		$exitCode = Remove-MSIApplications -Name "Java SE Development Kit 7 Update" -PassThru
+
+
+		$installedApplication = Get-InstalledApplication -Name 'NetBeans'
+		If ($installedApplication) {
+			If ($installedApplication.DisplayVersion -eq "8.1") {
+				[string]$installLocation = $installedApplication.InstallLocation.Trim('"')
+				Write-Log -Message "NetBeans was detected in $installLocation and will be removed." -Source $deployAppScriptFriendlyName
+				Remove-Folder -Path $installLocation
+				Remove-RegistryKey -Key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\nbi-nb-base-8.1.0.0.201510222201'
+				Remove-RegistryKey -Key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\nbi-nb-base-8.1.0.0.201510222201-1' -ContinueOnError $True
+				Remove-Folder -Path "${envCommonStartMenuPrograms}\NetBeans"
+			} Else {
+				Write-Log -Message "An unknown version of the application was found and hasn't been removed. Continuing to installation..." -Source $deployAppScriptFriendlyName
+			}
+		} Else {
+			Write-Log -Message "A previous installation not found. Continuing to installation..." -Source $deployAppScriptFriendlyName
+		}
+
+		##*===============================================
+		##* INSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Installation'
+
+		## Handle Zero-Config MSI Installations
+		If ($useDefaultMsi) {
+			[hashtable]$ExecuteDefaultMSISplat =  @{ Action = 'Install'; Path = $defaultMsiFile }; If ($defaultMstFile) { $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile) }
+			Execute-MSI @ExecuteDefaultMSISplat; If ($defaultMspFiles) { $defaultMspFiles | ForEach-Object { Execute-MSI -Action 'Patch' -Path $_ } }
+		}
+
+		## <Perform Installation tasks here>
+		$exitCode = Execute-Process -Path "jdk-8u151-nb-8_2-windows-x64.exe" -Parameters "--silent" -WindowStyle "Hidden" -WaitForMsiExec -PassThru
+
+
+		##*===============================================
+		##* POST-INSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Post-Installation'
+
+		## <Perform Post-Installation tasks here>
+		$exitCode = Execute-MSI -Action 'Uninstall' -Path '{4A03706F-666A-4037-7777-5F2748764D10}' -PassThru
+		Remove-File -Path "${envCommonDesktop}\NetBeans*.lnk" -ContinueOnError $true
+		Remove-File -Path "$envCommonStartMenuPrograms\Java\Check For Updates.lnk" -ContinueOnError $true
+
+		## Display a message at the end of the install
+		#If (-not $useDefaultMsi) { Show-InstallationPrompt -Message "${appVendor} ${appName} ${appVersion} was installed successfully." -ButtonRightText 'OK' -Icon Information -NoWait }
+	}
+	ElseIf ($deploymentType -ieq 'Uninstall')
+	{
+		##*===============================================
+		##* PRE-UNINSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Pre-Uninstallation'
+
+		## Show Welcome Message, close applications with a 60 second countdown before automatically closing
+		Show-InstallationWelcome -CloseApps 'iexplore,firefox,netbeans64' -CloseAppsCountdown 60
+
+		## Show Progress Message (with the default message)
+		Show-InstallationProgress
+
+		## <Perform Pre-Uninstallation tasks here>
+
+
+		##*===============================================
+		##* UNINSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Uninstallation'
+
+		## Handle Zero-Config MSI Uninstallations
+		If ($useDefaultMsi) {
+			[hashtable]$ExecuteDefaultMSISplat =  @{ Action = 'Uninstall'; Path = $defaultMsiFile }; If ($defaultMstFile) { $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile) }
+			Execute-MSI @ExecuteDefaultMSISplat
+		}
+
+		# <Perform Uninstallation tasks here>
+
+
+		##*===============================================
+		##* POST-UNINSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Post-Uninstallation'
+
+		## <Perform Post-Uninstallation tasks here>
+
+
+	}
+
+	##*===============================================
+	##* END SCRIPT BODY
+	##*===============================================
+
+	## Call the Exit-Script function to perform final cleanup operations
+	Exit-Script -ExitCode $mainExitCode
+}
+Catch {
+	[int32]$mainExitCode = 60001
+	[string]$mainErrorMessage = "$(Resolve-Error)"
+	Write-Log -Message $mainErrorMessage -Severity 3 -Source $deployAppScriptFriendlyName
+	Show-DialogBox -Text $mainErrorMessage -Icon 'Stop'
+	Exit-Script -ExitCode $mainExitCode
+}
